8001db8bd32c5ffd750ef206bc6582c51ef922a2
kyle belle <43228761+kyle-belle@users.noreply.github.com>
2022-06-14 04:55:32 -0400

    add attendance to class page student tab

--- src/Components/Class/Attendance.js
+++ src/Components/Class/Attendance.js
@@ -3,9 +3,29 @@ import React from 'react';
 import "./Attendance.css";
 
 const Attendance = ({_class={}, attendance=[]}) => {
+    const {students=[]} = _class;
     return (
-        <div>
+        <div className='attendance'>
             <h3>Attendance For Today</h3>
+            <ul className='attendance-list'>
+                {students.map((s) => {
+                    const {_id, name={}, email, phone} = s;
+
+                    return (
+                        <li className='attendance-container'>
+                            <p className='student-name'>{name.first} {name.last}</p>
+
+                            <div className='input-container attendance-select on-time'>
+                                <select>
+                                    <option>On time</option>
+                                    <option>Absent</option>
+                                    <option>Late</option>
+                                </select>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
